ba3b9636a11c1849f8457d8e19eeb4d988cb7b48
SeaSailors <130479086+SeaSailors@users.noreply.github.com>
2025-05-15 01:31:42 +0800

    chore: increase rpc read connection busy_timeout to 10s

--- sqlite_conn.go
+++ sqlite_conn.go
@@ -127,7 +127,7 @@ func (c *SqliteReadConn) ResetImmutableAfterVersionChanged() error {
 		return err
 	}
 
-	err = conn.Exec(fmt.Sprintf("PRAGMA busy_timeout=%d;", c.opts.BusyTimeout))
+	err = conn.Exec(fmt.Sprintf("PRAGMA busy_timeout=%d;", 10_000))
 	if err != nil {
 		return err
 	}
