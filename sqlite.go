eca77133d9ca812bdf4292bb08dea3a1bb509507
SeaSailors <130479086+SeaSailors@users.noreply.github.com>
2025-05-18 14:31:26 +0800

    chore(sqlite): set synchronous to FULL

--- sqlite.go
+++ sqlite.go
@@ -365,8 +365,7 @@ func (sql *SqliteDb) resetWriteConn() (err error) {
 		return err
 	}
 
-	err = sql.treeWrite.Exec("PRAGMA synchronous=OFF;")
-	// err = sql.treeWrite.Exec("PRAGMA synchronous=NORMAL;")
+	err = sql.treeWrite.Exec("PRAGMA synchronous=FULL;")
 	if err != nil {
 		return err
 	}
@@ -410,8 +409,7 @@ func (sql *SqliteDb) resetWriteConn() (err error) {
 		return err
 	}
 
-	err = sql.leafWrite.Exec("PRAGMA synchronous=OFF;")
-	// err = sql.leafWrite.Exec("PRAGMA synchronous=NORMAL;")
+	err = sql.leafWrite.Exec("PRAGMA synchronous=FULL;")
 	if err != nil {
 		return err
 	}
@@ -514,10 +512,6 @@ func (sql *SqliteDb) newReadConn() (*SqliteReadConn, error) {
 		return nil, err
 	}
 	err = conn.Exec("PRAGMA automatic_index=OFF;")
-	if err != nil {
-		return nil, err
-	}
-	err = conn.Exec("PRAGMA synchronous=OFF;")
 	if err != nil {
 		return nil, err
 	}
--- sqlite_conn.go
+++ sqlite_conn.go
@@ -73,12 +73,6 @@ func (c *SqliteReadConn) ResetToTreeVersion(version int64) error {
 		return err
 	}
 
-	err = conn.Exec("PRAGMA synchronous=OFF;")
-	if err != nil {
-		conn.Close()
-		return err
-	}
-
 	err = conn.Exec("PRAGMA automatic_index=OFF;")
 	if err != nil {
 		return err
