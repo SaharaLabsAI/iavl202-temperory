deed75c8dc1ff7e3104f26592f886a97fd3d76ee
SeaSailors <130479086+SeaSailors@users.noreply.github.com>
2025-05-20 14:32:05 +0800

    perf(import): fine tune

--- import.go
+++ import.go
@@ -6,7 +6,7 @@ import (
 )
 
 // maxBatchSize is the maximum size of the import batch before flushing it to the database
-const importBatchSize = 400_000
+const importBatchSize = 200_000
 
 // ErrNoImport is returned when calling methods on a closed importer
 var ErrNoImport = errors.New("no import in progress")
@@ -247,7 +247,7 @@ func (i *Importer) Commit() error {
 		return err
 	}
 
-	if err := i.tree.sql.treeWrite.Exec("PRAGMA wal_checkpoint(RESTART)"); err != nil {
+	if err := i.tree.sql.treeWrite.Exec("PRAGMA wal_checkpoint(TRUNCATE)"); err != nil {
 		return fmt.Errorf("failed tree checkpoint; %w", err)
 	}
 	err = i.tree.LoadVersion(i.version)
